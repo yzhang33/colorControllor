7cd7740776b27b82ac6c7cc99ac380c9ede61e28
Youming Zhang <yz33@youmings-air.myfiosgateway.com>
2021-04-07 10:26:17 -0400

    arduino

---
+++ arduino/mqtt.ino
@@ -0,0 +1,184 @@
+// Send temperature and humidity data to MQTT
+//
+// WiFiNINA https://www.arduino.cc/en/Reference/WiFiNINA (MKR WiFi 1010)
+// Arduino MQTT Client  https://github.com/arduino-libraries/ArduinoMqttClient
+// Adafruit DHT
+
+#include <WiFiNINA.h>
+#include <ArduinoMqttClient.h>
+#include <Wire.h>
+#include "Adafruit_MPR121.h"
+//touch sensor
+#ifndef _BV
+#define _BV(bit) (1 << (bit)) 
+#endif
+
+#include "config.h"
+
+#include <DHT.h>
+
+#define DHTPIN 3          // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22     // DHT 22  (AM2302), AM2321
+DHT dht(DHTPIN, DHTTYPE);
+
+WiFiSSLClient net;
+MqttClient mqtt(net);
+
+Adafruit_MPR121 cap = Adafruit_MPR121();
+uint16_t lasttouched = 0;
+uint16_t currtouched = 0;
+float humidity = 0;
+float lastHumidity=0;
+
+String temperatureTopic = "itp/" + DEVICE_ID + "/temperature";
+String humidityTopic = "itp/" + DEVICE_ID + "/humidity";
+String ledTopic = "itp/" + DEVICE_ID + "/led";
+
+// Publish every 10 seconds for the workshop. Real world apps need this data every 5 or 10 minutes.
+unsigned long publishInterval = 1000;
+unsigned long lastMillis = 0;
+const int ledPin = 13;
+
+void setup() {
+  Serial.begin(9600);
+
+  // Wait for a serial connection
+  //while (!Serial) { }
+  Serial.println("Adafruit MPR121 Capacitive Touch sensor test");
+  
+  if (!cap.begin(0x5A)) {
+    Serial.println("MPR121 not found, check wiring?");
+    while (1);
+  }
+  Serial.println("MPR121 found!");
+  
+  // initialize ledPin as an output.
+  pinMode(ledPin, OUTPUT);
+  
+  dht.begin();
+   
+  Serial.println("Connecting WiFi");
+  connectWiFi();
+
+  // set callback function for incoming MQTT messages
+  mqtt.onMessage(messageReceived);
+}
+
+void loop() {
+  if (WiFi.status() != WL_CONNECTED) {
+    connectWiFi();
+  }
+
+  if (!mqtt.connected()) {
+    connectMQTT();
+  }
+  
+  // poll for new MQTT messages and send keep alives
+  mqtt.poll();
+  currtouched = cap.touched();
+  
+  if (millis() - lastMillis > publishInterval) {
+    lastMillis = millis();
+
+    // read the sensor values
+    float temperature = dht.readTemperature(true);
+    //float humidity = dht.readHumidity();
+    
+    for (uint8_t i=0; i<12; i++) {
+      // it if *is* touched and *wasnt* touched before, alert!
+      if ((currtouched & _BV(i)) && !(lasttouched & _BV(i)) ) {
+        Serial.print(i); Serial.println(" touched");
+        humidity = i;
+      }
+      // if it *was* touched and now *isnt*, alert!
+      if (!(currtouched & _BV(i)) && (lasttouched & _BV(i)) ) {
+        Serial.print(i); Serial.println(" released");
+        humidity = 0-i;
+      }
+    }
+  
+    // reset our state
+    lasttouched = currtouched;
+    
+
+    Serial.print(temperature);
+    Serial.print("°F ");
+    Serial.print(humidity);
+    Serial.println("% RH");
+    
+    mqtt.beginMessage(temperatureTopic);
+    mqtt.print(temperature); 
+    mqtt.endMessage();
+
+    mqtt.beginMessage(humidityTopic);
+    mqtt.print(humidity); 
+    mqtt.endMessage();
+  }  
+}
+
+void connectWiFi() {
+  // Check for the WiFi module
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  Serial.print("WiFi firmware version ");
+  Serial.println(WiFi.firmwareVersion());
+  
+  Serial.print("Attempting to connect to SSID: ");
+  Serial.print(WIFI_SSID);
+  Serial.print(" ");
+
+  while (WiFi.begin(WIFI_SSID, WIFI_PASSWORD) != WL_CONNECTED) {
+    // failed, retry
+    Serial.print(".");
+    delay(3000);
+  }
+
+  Serial.println("Connected to WiFi");
+  printWiFiStatus();
+
+}
+
+void connectMQTT() {
+  Serial.print("Connecting MQTT broker ");
+  Serial.println(MQTT_BROKER);
+  mqtt.setId(DEVICE_ID);
+  mqtt.setUsernamePassword(MQTT_USER, MQTT_PASSWORD);
+
+  while (!mqtt.connect(MQTT_BROKER, MQTT_PORT)) {
+    Serial.print("Connection error ");
+    Serial.println(mqtt.connectError());
+    Serial.println("Waiting 5 seconds before retrying");
+    delay(5000);
+    // check the wifi before looping again
+    if (WiFi.status() != WL_CONNECTED) {
+      connectWiFi();
+    }
+  }
+
+  mqtt.subscribe(ledTopic);
+  Serial.println("connected.");
+}
+
+void printWiFiStatus() {
+  // print your WiFi IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+}
+
+void messageReceived(int messageSize) {
+  String topic = mqtt.messageTopic();
+  String payload = mqtt.readString();
+  Serial.println("incoming: " + topic + " - " + messageSize + " bytes ");
+  Serial.println(payload);
+  if (payload.equalsIgnoreCase("ON")) {
+    digitalWrite(ledPin, HIGH);
+  } else if (payload.equalsIgnoreCase("OFF")) {
+    digitalWrite(ledPin, LOW);    
+  } 
+}
